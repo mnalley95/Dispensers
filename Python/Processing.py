d5cf4a5b327075b65de6b988685a9cb3ea0a7616
Mark Nalley <mnalley95@gmail.com>
2021-04-10 12:14:22 -0400

    change freq to week

--- Python/Processing.py
+++ Python/Processing.py
@@ -30,15 +30,24 @@ grouped_df=raw_data.groupby(["Custname","Itemnmbr","Glpostdt"])["Quantity"].sum(
 #final formatting
 processed_df= grouped_df.reset_index()
 processed_df['Label'] = np.where(processed_df['Custname'].str.contains('com'), 'COM', np.where(processed_df['Custname'].str.contains('di') | processed_df['Custname'].str.contains('Direct Import'), 'DI', 'Domestic'))
-processed_df['sku'] = processed_df['Custname'] + '_' + processed_df['Itemnmbr'].astype('str')
-processed_df = processed_df.drop(['Custname', 'Itemnmbr'], axis = 1).rename({'Glpostdt': 'x', 'Quantity': 'y'}, axis = 1)
+processed_df['sku'] = processed_df['Itemnmbr'].astype('str')
+processed_df = processed_df.drop(['Itemnmbr'], axis = 1).rename({'Glpostdt': 'x', 'Quantity': 'y'}, axis = 1)
 
 #clean inactive products
 date_check = np.datetime64('2021-01-01')
-processed_df = processed_df.groupby(['sku']).filter(lambda d: max(d['x']) > date_check).reset_index(drop = True)
+processed_df = processed_df.groupby(['sku', 'Custname', 'Label']).filter(lambda d: max(d['x']) > date_check).reset_index(drop = True)
+
+#get groups with more than 6 rows
+group_size = processed_df.groupby(['Custname','Label','sku']).size()
+group_size = pd.DataFrame(group_size).rename({0:'size'}, axis=1)
+group_size = group_size[group_size['size'] > 12]
+
+#filter based upon index of group_size
+index1 = processed_df.set_index(['Custname', 'Label', 'sku']).index
+index2 = group_size.index
+
+processed_df = processed_df[index1.isin(index2)]
 # %%
 freq = 'W'
 
-processed_df_fill = fill_dt_all(processed_df, ts_id=['sku', 'Label'], dates = ('min', end_date, 'D'), freq = freq)
-
-# %%
+processed_df_fill = fill_dt_all(processed_df, ts_id=['sku', 'Label', 'Custname'], dates = ('min', end_date, 'D'), freq = freq)
